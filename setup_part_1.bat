09a1e209086fc1aa9119777bdca7a24db693016e
Kade <kaderabe04@gmail.com>
2022-05-26 18:37:26 -0700

    literally added one space

---
+++ setup_part_1.bat
@@ -0,0 +1,13 @@
+title setup part 1 for youtube downloader script by Kade Rabe
+
+echo setup part 1 for youtube downloader script by Kade Rabe
+
+pause
+
+python get-pip.py
+
+setup_part_1.bat
+
+echo pip installed run setup_part_2.bat next
+
+pause
