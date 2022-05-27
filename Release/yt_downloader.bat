37d0565045b68b25f0ae01c27c5bd37db1260de7
Kade <kaderabe04@gmail.com>
2022-05-26 18:22:48 -0700

    small .bat release fixes

--- Release/yt_downloader.bat
+++ Release/yt_downloader.bat
@@ -4,6 +4,8 @@ echo youtube video downloader script by Kade Rabe
 
 pause
 
-cd desktop/yt_downloader
+cd yt_downloader
 
 python main.py
+
+pause
--- Release/yt_downloader/setup_part_1.bat
+++ Release/yt_downloader/setup_part_1.bat
@@ -4,8 +4,6 @@ echo setup part 1 for youtube downloader script by Kade Rabe
 
 pause
 
-cd desktop/yt_downloader
-
 python get-pip.py
 
 pause
